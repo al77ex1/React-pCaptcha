1b552113304a5145c2a974b2c124a994e18e693b
Alex Shatunov <al77ex1@gmail.com>
2021-11-23 22:31:54 +0300

    modified route get-captcha

--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
 app.get('/get-captcha', (req, res) => {
-  res.send('Successful response.');
+
+  // generating a set of pictures
+  const numbers = [];
+  while(numbers.length < 15){
+    const number = Math.floor(Math.random() * 113) + 1;
+    if(numbers.indexOf(number) === -1) numbers.push(number);
+  }
+
+  // generating a selected images
+  const select = [];
+  while(select.length < 3){
+    let number = Math.floor(Math.random() * 14) + 1;
+    number = numbers[number];
+    if(select.indexOf(number) === -1) select.push(number);
+  }
+
+  // reading selected images into base64
+  const pictures = [];
+  select.forEach(number => {
+    pictures.push(fs.readFileSync(`./collection/${number}.png`, {encoding: 'base64'}));
+  })
+
+  // writing the result
+  const timeStamp = Math.floor(Date.now() / 1000);
+  fs.appendFileSync(`./results/${timeStamp}`, `${select.join(' ')}`);
+
+  res.send({numbers, pictures, timeStamp});
 });
 
 app.get('/check-captcha', (req, res) => {
