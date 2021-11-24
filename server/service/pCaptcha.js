0cfb238844c3208e5e2f59727e9e8afd7e583fc2
Alex Shatunov <al77ex1@gmail.com>
2021-11-24 10:49:41 +0300

    added constants

--- server/service/pCaptcha.js
+++ server/service/pCaptcha.js
@@ -1,19 +1,24 @@
 const fs = require("fs");
 const findRemoveSync = require("find-remove");
 
+const amountFiles = 113;
+const collectionSize = 15;
+const selected = 3;
+const resultExpiration = 30;
+
 const getCaptcha = () => {
 
     // generating a set of pictures
     const numbers = [];
-    while(numbers.length < 15){
-        const number = Math.floor(Math.random() * 113) + 1;
+    while(numbers.length < collectionSize){
+        const number = Math.floor(Math.random() * amountFiles) + 1;
         if(numbers.indexOf(number) === -1) numbers.push(number);
     }
 
     // generating a selected images
     const select = [];
-    while(select.length < 3){
-        let number = Math.floor(Math.random() * 14) + 1;
+    while(select.length < selected){
+        let number = Math.floor(Math.random() * (collectionSize - 1)) + 1;
         number = numbers[number];
         if(select.indexOf(number) === -1) select.push(number);
     }
@@ -29,14 +34,14 @@ const getCaptcha = () => {
     fs.writeFileSync(`${__dirname}/results/${timeStamp}.chk`, `${select.join(' ')}`);
 
     // removing old results
-    findRemoveSync(`${__dirname}/results`, {age: { seconds: 300 }, extensions: '.chk'});
+    findRemoveSync(`${__dirname}/results`, {age: { seconds: resultExpiration }, extensions: '.chk'});
 
     return {numbers, pictures, timeStamp};
 }
 
 const checkCaptcha = (timeStamp, choice) => {
     // removing old results
-    findRemoveSync(`${__dirname}/results`, {age: { seconds: 300 }, extensions: '.chk'});
+    findRemoveSync(`${__dirname}/results`, {age: { seconds: resultExpiration }, extensions: '.chk'});
 
     if (!choice || !timeStamp) return {status: 'failed', message: 'The "choice" and "timeStamp" parameters is required.'}
     if (!fs.existsSync(`${__dirname}/results/${timeStamp}.chk`)) return {status: 'failed', message: 'Captcha is out of date.'}
